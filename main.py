6e9e355fca820ab859025e68421cac72715971b9
rururu <zspovenetsky@gmail.com>
2025-06-01 14:14:24 +0300

    Pauses between starting processes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process
+import time
 
 from CLIPSServer import start_CLIPSServer
 from NMEAServer import start_NMEAServer
@@ -7,6 +8,9 @@ from webbrowser import open
 
 if __name__ == '__main__':
     Process(target=start_CLIPSServer).start()
+    time.sleep(2)
     Process(target=start_NMEAServer).start()
+    time.sleep(2)
     Process(target=start_DisplayServer).start()
+    time.sleep(2)
     Process(target=open, args=("http://localhost:8448/chart",)).start()
